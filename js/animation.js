7b040119cfb7949a89baee68dcf59c3ef33b747d
Michael Alan <aran.arukado@outlook.com>
2017-03-03 05:17:48 +0700

    fixed late header fix position

--- js/animation.js
+++ js/animation.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
   });
 
   $(window).scroll(function(){
-    var shrinkOn=$("#header").height(),paddingAdd=$("#header").height();
-    if($(window).scrollTop()>shrinkOn+12)
+    var shrinkOn=$("#header").height(), paddingAdd=$("#header").height()-2;
+    if($(window).scrollTop()>shrinkOn+11)
     {
       $("#menu").css({'position':'fixed'});
       $("#theme").css({'margin-top':paddingAdd+'px'});
